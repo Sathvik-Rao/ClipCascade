f51f5209065077fbe755270460e242397efb062c
Sathvik Rao Poladi <36164509+Sathvik-Rao@users.noreply.github.com>
2024-11-05 21:22:55 -0500

    code: Initial commit for macOS implementation

---
+++ ClipCascade_Desktop/clipcascade_macos/src/gui/login.py
@@ -0,0 +1,131 @@
+import re
+import tkinter as tk
+
+
+class LoginForm(tk.Tk):
+    def __init__(
+        self,
+        username,
+        password,
+        server_url,
+        websocket_url,
+        cipher_enabled,
+        notification,
+        on_login_callback=None,
+        on_quit_callback=None,
+    ):
+        super().__init__()
+        self.on_login_callback = on_login_callback
+        self.on_quit_callback = on_quit_callback
+
+        self.withdraw()  # Hide the root window
+        self.title("ClipCascade")
+
+        # Set window size and center it
+        window_width = 500
+        window_height = 300
+        screen_width = self.winfo_screenwidth()
+        screen_height = self.winfo_screenheight()
+        x = (screen_width // 2) - (window_width // 2)
+        y = (screen_height // 2) - (window_height // 2)
+        self.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+        # Create a frame for the fields and arrange them in two columns
+        self.field_frame = tk.Frame(self)
+        self.field_frame.pack(padx=20, pady=20, fill=tk.X)
+
+        # Username Field
+        tk.Label(self.field_frame, text="Username").grid(
+            row=0, column=0, padx=10, pady=5, sticky=tk.W
+        )
+        self.username_entry = tk.Entry(self.field_frame, width=50)
+        self.username_entry.insert(0, username)
+        self.username_entry.grid(row=0, column=1, padx=10, pady=5, sticky=tk.W + tk.E)
+
+        # Password Field
+        tk.Label(self.field_frame, text="Password").grid(
+            row=1, column=0, padx=10, pady=5, sticky=tk.W
+        )
+        self.password_frame = tk.Frame(self.field_frame)
+        self.password_frame.grid(row=1, column=1, padx=10, pady=5, sticky=tk.W + tk.E)
+        self.password_entry = tk.Entry(self.password_frame, show="*")
+        self.password_entry.insert(0, password)
+        self.password_entry.pack(side=tk.LEFT, fill=tk.X, expand=True)
+        self.eye_icon = tk.Label(self.password_frame, text="🙈", cursor="hand2")
+        self.eye_icon.pack(side=tk.RIGHT)
+        self.eye_icon.bind("<Button-1>", self.toggle_password)
+
+        # Server URL Field
+        tk.Label(self.field_frame, text="Server URL").grid(
+            row=2, column=0, padx=10, pady=5, sticky=tk.W
+        )
+        self.server_url_entry = tk.Entry(self.field_frame)
+        self.server_url_entry.insert(0, server_url)
+        self.server_url_entry.grid(row=2, column=1, padx=10, pady=5, sticky=tk.W + tk.E)
+
+        # WebSocket URL Field
+        tk.Label(self.field_frame, text="WebSocket URL").grid(
+            row=3, column=0, padx=10, pady=5, sticky=tk.W
+        )
+        self.websocket_url_entry = tk.Entry(self.field_frame)
+        self.websocket_url_entry.insert(0, websocket_url)
+        self.websocket_url_entry.grid(
+            row=3, column=1, padx=10, pady=5, sticky=tk.W + tk.E
+        )
+
+        # Enable Cipher Checkbox
+        self.cipher_var = tk.BooleanVar(value=cipher_enabled)
+        self.cipher_checkbox = tk.Checkbutton(
+            self, text="Enable Encryption(recommended)", variable=self.cipher_var
+        )
+        self.cipher_checkbox.pack(pady=3)
+
+        # Enable Notification Checkbox
+        self.notification_var = tk.BooleanVar(value=notification)
+        self.notification_checkbox = tk.Checkbutton(
+            self, text="Enable Notification", variable=self.notification_var
+        )
+        self.notification_checkbox.pack(pady=3)
+
+        # Login Button
+        self.login_button = tk.Button(self, text="Login", command=self.on_login)
+        self.login_button.pack(pady=10)
+
+        # Handle window close
+        self.protocol("WM_DELETE_WINDOW", self.on_quit)
+
+        self.deiconify()  # Show the root window
+
+    def toggle_password(self, event=None):
+        if self.password_entry.cget("show") == "*":
+            self.password_entry.config(show="")
+            self.eye_icon.config(text="🐵")
+        else:
+            self.password_entry.config(show="*")
+            self.eye_icon.config(text="🙈")
+
+    def on_login(self):
+        username = self.username_entry.get()
+        password = self.password_entry.get()
+        server_url = self.server_url_entry.get().strip()
+        server_url = re.sub(r"/+$", "", server_url)
+        websocket_url = self.websocket_url_entry.get().strip()
+        websocket_url = re.sub(r"/+$", "", websocket_url)
+        cipher_enabled = self.cipher_var.get()
+        notification = self.notification_var.get()
+
+        if self.on_login_callback:
+            self.on_login_callback(
+                username,
+                password,
+                server_url,
+                websocket_url,
+                cipher_enabled,
+                notification,
+            )
+        self.destroy()
+
+    def on_quit(self):
+        if self.on_quit_callback:
+            self.on_quit_callback()
+        self.destroy()
---
+++ ClipCascade_Desktop/clipcascade_macos/src/stomp_ws/client.py
@@ -0,0 +1,198 @@
+import time
+from threading import Thread
+
+from .frame import Frame
+import websocket
+import logging
+
+VERSIONS = "1.0,1.1"
+
+
+class Client:
+
+    def __init__(self, url, headers={}, on_close_callback=None):
+
+        self.url = url
+        self.ws = websocket.WebSocketApp(self.url, headers)
+        self.ws.on_open = self._on_open
+        self.ws.on_message = self._on_message
+        self.ws.on_error = self._on_error
+        self.ws.on_close = self._on_close
+        self.on_close_callback = on_close_callback
+
+        self.opened = False
+
+        self.connected = False
+
+        self.counter = 0
+        self.subscriptions = {}
+
+        self._connectCallback = None
+        self.errorCallback = None
+
+    def _connect(self, timeout=0):
+        thread = Thread(target=self.ws.run_forever)
+        thread.daemon = True
+        thread.start()
+
+        total_ms = 0
+        while self.opened is False:
+            time.sleep(0.25)
+            total_ms += 250
+            if 0 < timeout < total_ms:
+                raise TimeoutError(f"Connection to {self.url} timed out")
+
+    def _on_open(self, ws_app, *args):
+        self.opened = True
+
+    def _on_close(self, ws_app, *args):
+        self.connected = False
+        logging.debug("Whoops! Lost connection to " + self.ws.url)
+        if self.on_close_callback is not None:
+            self.on_close_callback()
+        self._clean_up()
+
+    def _on_error(self, ws_app, error, *args):
+        logging.debug(error)
+
+    def _on_message(self, ws_app, message, *args):
+        if message == "\n":  # If message is a newline, it's a heartbeat frame
+            logging.debug("Received heartbeat frame")
+            self.ws.send("\n")  # Send a heartbeat back to the server
+            logging.debug("Sent heartbeat frame")
+            return
+
+        logging.debug("\n<<< " + str(message))
+        frame = Frame.unmarshall_single(message)
+        _results = []
+        if frame.command == "CONNECTED":
+            self.connected = True
+            logging.debug("connected to server " + self.url)
+            if self._connectCallback is not None:
+                _results.append(self._connectCallback(frame))
+        elif frame.command == "MESSAGE":
+
+            subscription = frame.headers["subscription"]
+
+            if subscription in self.subscriptions:
+                onreceive = self.subscriptions[subscription]
+                messageID = frame.headers["message-id"]
+
+                def ack(headers):
+                    if headers is None:
+                        headers = {}
+                    return self.ack(messageID, subscription, headers)
+
+                def nack(headers):
+                    if headers is None:
+                        headers = {}
+                    return self.nack(messageID, subscription, headers)
+
+                frame.ack = ack
+                frame.nack = nack
+
+                _results.append(onreceive(frame))
+            else:
+                info = "Unhandled received MESSAGE: " + str(frame)
+                logging.debug(info)
+                _results.append(info)
+        elif frame.command == "RECEIPT":
+            pass
+        elif frame.command == "ERROR":
+            if self.errorCallback is not None:
+                _results.append(self.errorCallback(frame))
+        else:
+            info = "Unhandled received MESSAGE: " + frame.command
+            logging.debug(info)
+            _results.append(info)
+
+        return _results
+
+    def _transmit(self, command, headers, body=None):
+        out = Frame.marshall(command, headers, body)
+        logging.debug("\n>>> " + out)
+        self.ws.send(out)
+
+    def connect(
+        self,
+        login=None,
+        passcode=None,
+        headers=None,
+        connectCallback=None,
+        errorCallback=None,
+        timeout=0,
+    ):
+
+        logging.debug("Opening web socket...")
+        self._connect(timeout)
+
+        headers = headers if headers is not None else {}
+        headers["host"] = self.url
+        headers["accept-version"] = VERSIONS
+        headers["heart-beat"] = "10000,10000"
+
+        if login is not None:
+            headers["login"] = login
+        if passcode is not None:
+            headers["passcode"] = passcode
+
+        self._connectCallback = connectCallback
+        self.errorCallback = errorCallback
+
+        self._transmit("CONNECT", headers)
+
+    def disconnect(self, disconnectCallback=None, headers=None):
+        if headers is None:
+            headers = {}
+
+        self._transmit("DISCONNECT", headers)
+        self.ws.on_close = None
+        self.ws.close()
+        self._clean_up()
+
+        if disconnectCallback is not None:
+            disconnectCallback()
+
+    def _clean_up(self):
+        self.connected = False
+
+    def send(self, destination, headers=None, body=None):
+        if headers is None:
+            headers = {}
+        if body is None:
+            body = ""
+        headers["destination"] = destination
+        return self._transmit("SEND", headers, body)
+
+    def subscribe(self, destination, callback=None, headers=None):
+        if headers is None:
+            headers = {}
+        if "id" not in headers:
+            headers["id"] = "sub-" + str(self.counter)
+            self.counter += 1
+        headers["destination"] = destination
+        self.subscriptions[headers["id"]] = callback
+        self._transmit("SUBSCRIBE", headers)
+
+        def unsubscribe():
+            self.unsubscribe(headers["id"])
+
+        return headers["id"], unsubscribe
+
+    def unsubscribe(self, id):
+        del self.subscriptions[id]
+        return self._transmit("UNSUBSCRIBE", {"id": id})
+
+    def ack(self, message_id, subscription, headers):
+        if headers is None:
+            headers = {}
+        headers["message-id"] = message_id
+        headers["subscription"] = subscription
+        return self._transmit("ACK", headers)
+
+    def nack(self, message_id, subscription, headers):
+        if headers is None:
+            headers = {}
+        headers["message-id"] = message_id
+        headers["subscription"] = subscription
+        return self._transmit("NACK", headers)
